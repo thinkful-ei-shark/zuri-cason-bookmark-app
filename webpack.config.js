f482db2cabef051a03eea4e9d642b4d584fb6a4d
zcason <zuri.cason@gmail.com>
2020-10-18 13:39:20 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: __dirname + '/dist',
+        path: __dirname,
         filename: 'index_bundle.js'
     },
     mode: 'development',
